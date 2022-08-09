d773cd24408c1fd3282361f32a6fd5ae88f8b16a
Vairamuthu <mvairamuthu2003@gmail.com>
2022-08-10 00:20:10 +0530

    updated Footer links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,71 +1,71 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import {
-	AiFillGithub,
-	AiOutlineTwitter,
-	AiFillInstagram,
+  AiFillGithub,
+  AiOutlineTwitter,
+  AiFillInstagram,
 } from "react-icons/ai";
 import { FaLinkedinIn } from "react-icons/fa";
 
 function Footer() {
-	let date = new Date();
-	let year = date.getFullYear();
-	return (
-		<Container fluid className="footer">
-			<Row>
-				<Col md="4" className="footer-copywright">
-					<h3>Designed and Developed by Vm</h3>
-				</Col>
-				<Col md="4" className="footer-copywright">
-					<h3>Copyright © {year} Vm</h3>
-				</Col>
-				<Col md="4" className="footer-body">
-					<ul className="footer-icons">
-						<li className="social-icons">
-							<a
-								href="https://github.com/vmmuthu31"
-								style={{ color: "white" }}
-								target="_blank"
-								rel="noopener noreferrer"
-							>
-								<AiFillGithub />
-							</a>
-						</li>
-						<li className="social-icons">
-							<a
-								href="https://twitter.com/Vairamu56935830"
-								style={{ color: "white" }}
-								target="_blank"
-								rel="noopener noreferrer"
-							>
-								<AiOutlineTwitter />
-							</a>
-						</li>
-						<li className="social-icons">
-							<a
-								href="https://www.linkedin.com/in/vairamuthu-muthu-10a3651b3/"
-								style={{ color: "white" }}
-								target="_blank"
-								rel="noopener noreferrer"
-							>
-								<FaLinkedinIn />
-							</a>
-						</li>
-						<li className="social-icons">
-							<a
-								href="https://www.instagram.com/barfi_31_/"
-								style={{ color: "white" }}
-								target="_blank"
-								rel="noopener noreferrer"
-							>
-								<AiFillInstagram />
-							</a>
-						</li>
-					</ul>
-				</Col>
-			</Row>
-		</Container>
-	);
+  let date = new Date();
+  let year = date.getFullYear();
+  return (
+    <Container fluid className="footer">
+      <Row>
+        <Col md="4" className="footer-copywright">
+          <h3>Designed and Developed by Vm</h3>
+        </Col>
+        <Col md="4" className="footer-copywright">
+          <h3>Copyright © {year} Vm</h3>
+        </Col>
+        <Col md="4" className="footer-body">
+          <ul className="footer-icons">
+            <li className="social-icons">
+              <a
+                href="https://github.com/vmmuthu31"
+                style={{ color: "white" }}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <AiFillGithub />
+              </a>
+            </li>
+            <li className="social-icons">
+              <a
+                href="https://twitter.com/Vairamu56935830"
+                style={{ color: "white" }}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <AiOutlineTwitter />
+              </a>
+            </li>
+            <li className="social-icons">
+              <a
+                href="https://www.linkedin.com/in/vmmuthu31/"
+                style={{ color: "white" }}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FaLinkedinIn />
+              </a>
+            </li>
+            <li className="social-icons">
+              <a
+                href="https://www.instagram.com/barfi_31_/"
+                style={{ color: "white" }}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <AiFillInstagram />
+              </a>
+            </li>
+          </ul>
+        </Col>
+      </Row>
+    </Container>
+  );
 }
 
 export default Footer;
--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -90,7 +90,7 @@ function Home2() {
               </li>
               <li className="social-icons">
                 <a
-                  href="https://www.linkedin.com/in/vairamuthu-muthu-10a3651b3/"
+                  href="https://www.linkedin.com/in/vmmuthu31/"
                   target="_blank"
                   rel="noreferrer"
                   className="icon-colour  home-social-icons"
