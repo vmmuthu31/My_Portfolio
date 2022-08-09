a3e6fc9aed0228c96a01395d6895016c04673e03
Vairamuthu <mvairamuthu2003@gmail.com>
2022-08-10 00:08:48 +0530

    updated fontcolor of aboutjs

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -3,39 +3,37 @@ import Card from "react-bootstrap/Card";
 import { ImPointRight } from "react-icons/im";
 
 function AboutCard() {
-	return (
-		<Card className="quote-card-view">
-			<Card.Body>
-				<blockquote className="blockquote mb-0">
-					<p style={{ textAlign: "justify" }}>
-						Hi Everyone, I am <span className="purple">Vairamuthu </span>
-						from <span className="purple"> Chennai, India.</span>
-						<br />I am pursuing B.E in Computer Science Engineering at Jaya
-						Engineering College.
-						<br />
-						<br />
-						Apart from coding, some other activities that I love to do!
-					</p>
-					<ul>
-						<li className="about-activity">
-							<ImPointRight /> Playing Games
-						</li>
-						<li className="about-activity">
-							<ImPointRight /> Watching Movies
-						</li>
-						<li className="about-activity">
-							<ImPointRight /> Travelling
-						</li>
-					</ul>
+  return (
+    <Card className="quote-card-view">
+      <Card.Body>
+        <blockquote className="blockquote mb-0">
+          <p style={{ textAlign: "justify" }}>
+            Hi Everyone, I am <span className="purple">Vairamuthu </span>
+            from <span className="purple"> Chennai, India.</span>
+            <br />I am pursuing B.E in Computer Science Engineering at Jaya
+            Engineering College.
+            <br />
+            <br />
+            Apart from coding, some other activities that I love to do!
+          </p>
+          <ul>
+            <li className="about-activity">
+              <ImPointRight /> Playing Games
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> Watching Movies
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> Travelling
+            </li>
+          </ul>
 
-					<p style={{ color: "rgb(155 126 172)" }}>
-						"Live the life like you love!"{" "}
-					</p>
-					<footer className="blockquote-footer">Vairamuthu</footer>
-				</blockquote>
-			</Card.Body>
-		</Card>
-	);
+          <p className="purple">"Live the life like you love!" </p>
+          <footer className="blockquote-footer">Vairamuthu</footer>
+        </blockquote>
+      </Card.Body>
+    </Card>
+  );
 }
 
 export default AboutCard;
