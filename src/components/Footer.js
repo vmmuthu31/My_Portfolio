6ca3f147c32ad90346b38fff3399ec766794e87f
Vairamuthu <mvairamuthu2003@gmail.com>
2022-11-18 14:33:09 +0530

    updated v2

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -17,7 +17,7 @@ function Footer() {
           <h3>Designed and Developed by Vm</h3>
         </Col>
         <Col md="4" className="footer-copywright">
-          <h3>Copyright © {year} Vm</h3>
+          <h3>Copyright ©{year} Vm</h3>
         </Col>
         <Col md="4" className="footer-body">
           <ul className="footer-icons">
