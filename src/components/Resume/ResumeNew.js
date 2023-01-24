51cd87606f23caaa4e56691658e8b66ec81784d1
Vairamuthu <mvairamuthu2003@gmail.com>
2023-01-24 14:16:53 +0530

    updated the project card

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -8,8 +8,7 @@ import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
-const resumeLink =
-  "https://raw.githubusercontent.com/vmmuthu31/My_Portfolio/master/src/Assets/MyResume.pdf";
+const resumeLink = pdf;
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
