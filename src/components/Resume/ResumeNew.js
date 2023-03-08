da681f1a3966448604dc0118c0f1a508e8377844
Vairamuthu <mvairamuthu2003@gmail.com>
2023-03-08 15:36:27 +0530

    updated the resume

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/../Assets/MyResume.pdf";
+import pdf from "../../Assets/../Assets/Resume_By_Vm.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
