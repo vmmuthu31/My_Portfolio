1359f05f681791d63d53072c1ec1ba967cecabd2
Vairamuthu <mvairamuthu2003@gmail.com>
2023-03-08 15:44:08 +0530

    updated the project section

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -30,6 +30,15 @@ function Projects() {
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
           <Col md={4} className="project-card">
             <ProjectCard
+              imgPath="https://user-images.githubusercontent.com/88650559/220404397-184f52a2-616c-46ed-bc12-d6ab77c15725.png"
+              isBlog={false}
+              title="Agri-Farm"
+              description=" Supply Chain Management (SCM) is an essential business process in all spheres of the economy. SCM uses speciﬁc processes to connect from producer to consumer requirement through a chain. In a BCT(Blockchain Technology) based system, “records are immutable and trusted, eliminating the need for third parties to be involved. Potential farmer-facing impacts include ensuring that farmers receive timely and complete payments through the use of smart contracts and helping farmers to capture real-time data to more effectively manage their crops and harvests.!"
+              link="https://github.com/vmmuthu31/Agri-Farm"
+            />
+          </Col>
+          <Col md={4} className="project-card">
+            <ProjectCard
               imgPath="https://user-images.githubusercontent.com/88650559/212527479-319e977f-3524-456f-af43-9996132f165a.png"
               isBlog={false}
               title="Matic-Name-Service"
