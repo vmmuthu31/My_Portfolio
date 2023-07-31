cb9ca31d72a5024cf891d555c37c58f7e87ef4c4
Vairamuthu <mvairamuthu2003@gmail.com>
2023-07-31 11:32:31 +0530

    updated the linkedin url

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -43,7 +43,7 @@ function Footer() {
             </li>
             <li className="social-icons">
               <a
-                href="https://www.linkedin.com/in/vmmuthu31/"
+                href="https://www.linkedin.com/in/vmmuthu/"
                 style={{ color: "white" }}
                 target="_blank"
                 rel="noopener noreferrer"
--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -92,7 +92,7 @@ function Home2() {
               </li>
               <li className="social-icons">
                 <a
-                  href="https://www.linkedin.com/in/vmmuthu31/"
+                  href="https://www.linkedin.com/in/vmmuthu/"
                   target="_blank"
                   rel="noreferrer"
                   className="icon-colour  home-social-icons"
