5c7eaf04408e9e6205fe9e313447f7ad7597a2bf
Vairamuthu <mvairamuthu2003@gmail.com>
2026-01-03 14:10:23 +0530

    refactor: remove Next.js dependency and dynamic NavBar import, and downgrade react-parallax-tilt

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,22 +1,15 @@
-import dynamic from 'next/dynamic'
-import {createContext} from 'react'
-import useTheme from './useTheme'
+import { createContext } from "react";
+import useTheme from "./useTheme";
+import NavBar from "./Navbar";
 
-const NavBar = dynamic(
-  () => import('./Navbar'),
-  {
-    ssr: false,
-  }
-)
-
-export const ThemeContext = createContext()
+export const ThemeContext = createContext();
 export default function Layout(props) {
-  const {children} = props
+  const { children } = props;
 
   return (
     <ThemeContext.Provider value={useTheme()}>
       <NavBar />
       <main>{children}</main>
     </ThemeContext.Provider>
-  )
+  );
 }
