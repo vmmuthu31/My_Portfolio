774d04cb88cb40a08b50b1445b554ca906a419dd
Vairamuthu <mvairamuthu2003@gmail.com>
2026-01-03 14:08:00 +0530

    feat: upgrade Next.js to 15.4.10, migrate to Bun for dependency management, and remove unused Navbar imports.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,131 +1,130 @@
-import React, { useState , useContext} from "react";
+import { useState } from "react";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import Container from "react-bootstrap/Container";
 import { Link } from "react-router-dom";
 import { GrCertificate } from "react-icons/gr";
-import { MoonIcon, SunIcon} from '@heroicons/react/outline';
 import { CgGitFork } from "react-icons/cg";
 import Button from "react-bootstrap/Button";
 import {
-	AiFillStar,
-	AiOutlineHome,
-	AiOutlineFundProjectionScreen,
-	AiOutlineUser,
+  AiFillStar,
+  AiOutlineHome,
+  AiOutlineFundProjectionScreen,
+  AiOutlineUser,
 } from "react-icons/ai";
 import { CgFileDocument } from "react-icons/cg";
 
 function NavBar() {
-	const [expand, updateExpanded] = useState(false);
-	const [navColour, updateNavbar] = useState(false);
+  const [expand, updateExpanded] = useState(false);
+  const [navColour, updateNavbar] = useState(false);
 
-	function scrollHandler() {
-		if (window.scrollY >= 20) {
-			updateNavbar(true);
-		} else {
-			updateNavbar(false);
-		}
-	}
+  function scrollHandler() {
+    if (window.scrollY >= 20) {
+      updateNavbar(true);
+    } else {
+      updateNavbar(false);
+    }
+  }
 
-	window.addEventListener("scroll", scrollHandler);
-	const certi = {
-		marginBottom: "2px",
-		background: "white",
-		fontSize: "1.1em",
-	};
+  window.addEventListener("scroll", scrollHandler);
+  const certi = {
+    marginBottom: "2px",
+    background: "white",
+    fontSize: "1.1em",
+  };
 
-	return (
-		<Navbar
-			expanded={expand}
-			fixed="top"
-			expand="md"
-			className={navColour ? "sticky" : "navbar"}
-		>
-			<Container>
-				<Navbar.Brand href="/" className="d-flex">
-					<h1
-						style={{ color: "#white", marginTop: "10px" }}
-						className="img-fluid logo"
-						alt="brand"
-					>
-						My Portfolio
-					</h1>
-				</Navbar.Brand>
-				<Navbar.Toggle
-					aria-controls="responsive-navbar-nav"
-					onClick={() => {
-						updateExpanded(expand ? false : "expanded");
-					}}
-				>
-					<span></span>
-					<span></span>
-					<span></span>
-				</Navbar.Toggle>
-				<Navbar.Collapse id="responsive-navbar-nav">
-					<Nav className="ms-auto" defaultActiveKey="#home">
-						<Nav.Item>
-							<Nav.Link as={Link} to="/" onClick={() => updateExpanded(false)}>
-								<AiOutlineHome style={{ marginBottom: "2px" }} /> Home
-							</Nav.Link>
-						</Nav.Item>
+  return (
+    <Navbar
+      expanded={expand}
+      fixed="top"
+      expand="md"
+      className={navColour ? "sticky" : "navbar"}
+    >
+      <Container>
+        <Navbar.Brand href="/" className="d-flex">
+          <h1
+            style={{ color: "#white", marginTop: "10px" }}
+            className="img-fluid logo"
+            alt="brand"
+          >
+            My Portfolio
+          </h1>
+        </Navbar.Brand>
+        <Navbar.Toggle
+          aria-controls="responsive-navbar-nav"
+          onClick={() => {
+            updateExpanded(expand ? false : "expanded");
+          }}
+        >
+          <span></span>
+          <span></span>
+          <span></span>
+        </Navbar.Toggle>
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="ms-auto" defaultActiveKey="#home">
+            <Nav.Item>
+              <Nav.Link as={Link} to="/" onClick={() => updateExpanded(false)}>
+                <AiOutlineHome style={{ marginBottom: "2px" }} /> Home
+              </Nav.Link>
+            </Nav.Item>
 
-						<Nav.Item>
-							<Nav.Link
-								as={Link}
-								to="/about"
-								onClick={() => updateExpanded(false)}
-							>
-								<AiOutlineUser style={{ marginBottom: "2px" }} /> About
-							</Nav.Link>
-						</Nav.Item>
+            <Nav.Item>
+              <Nav.Link
+                as={Link}
+                to="/about"
+                onClick={() => updateExpanded(false)}
+              >
+                <AiOutlineUser style={{ marginBottom: "2px" }} /> About
+              </Nav.Link>
+            </Nav.Item>
 
-						<Nav.Item>
-							<Nav.Link
-								as={Link}
-								to="/project"
-								onClick={() => updateExpanded(false)}
-							>
-								<AiOutlineFundProjectionScreen
-									style={{ marginBottom: "2px" }}
-								/>{" "}
-								Projects
-							</Nav.Link>
-						</Nav.Item>
+            <Nav.Item>
+              <Nav.Link
+                as={Link}
+                to="/project"
+                onClick={() => updateExpanded(false)}
+              >
+                <AiOutlineFundProjectionScreen
+                  style={{ marginBottom: "2px" }}
+                />{" "}
+                Projects
+              </Nav.Link>
+            </Nav.Item>
 
-						<Nav.Item>
-							<Nav.Link
-								as={Link}
-								to="/resume"
-								onClick={() => updateExpanded(false)}
-							>
-								<CgFileDocument style={{ marginBottom: "2px" }} /> Resume
-							</Nav.Link>
-						</Nav.Item>
+            <Nav.Item>
+              <Nav.Link
+                as={Link}
+                to="/resume"
+                onClick={() => updateExpanded(false)}
+              >
+                <CgFileDocument style={{ marginBottom: "2px" }} /> Resume
+              </Nav.Link>
+            </Nav.Item>
 
-						<Nav.Item>
-							<Nav.Link
-								as={Link}
-								to="/certificates"
-								onClick={() => updateExpanded(false)}
-							>
-								<GrCertificate color="white" style={certi} /> Certificates
-							</Nav.Link>
-						</Nav.Item>
-						<Nav.Item className="fork-btn">
-						<Button
-							href="https://github.com/vmmuthu31/My_Portfolio"
-							target="_blank"
-							className="fork-btn-inner"
-						>
-							<CgGitFork style={{ fontSize: "1.2em" }} />{" "}
-							<AiFillStar style={{ fontSize: "1.1em" }} />
-						</Button>
-					</Nav.Item>
-					</Nav>
-				</Navbar.Collapse>
-			</Container>
-		</Navbar>
-	);
+            <Nav.Item>
+              <Nav.Link
+                as={Link}
+                to="/certificates"
+                onClick={() => updateExpanded(false)}
+              >
+                <GrCertificate color="white" style={certi} /> Certificates
+              </Nav.Link>
+            </Nav.Item>
+            <Nav.Item className="fork-btn">
+              <Button
+                href="https://github.com/vmmuthu31/My_Portfolio"
+                target="_blank"
+                className="fork-btn-inner"
+              >
+                <CgGitFork style={{ fontSize: "1.2em" }} />{" "}
+                <AiFillStar style={{ fontSize: "1.1em" }} />
+              </Button>
+            </Nav.Item>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 }
 
 export default NavBar;
