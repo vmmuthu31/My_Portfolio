dd3184c73187af97fa28d9ee59505308fdc32856
Vairamuthu <mvairamuthu2003@gmail.com>
2026-01-03 14:11:37 +0530

    feat: Dynamically import NavBar client-side and update Next.js along with other dependencies.

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,6 +1,10 @@
 import { createContext } from "react";
 import useTheme from "./useTheme";
-import NavBar from "./Navbar";
+import dynamic from "next/dynamic";
+
+const NavBar = dynamic(() => import("./Navbar"), {
+  ssr: false,
+});
 
 export const ThemeContext = createContext();
 export default function Layout(props) {
